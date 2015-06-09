04196e875475d235d7e3ccc04fa86e7ec9a18b02
Luís Dias <lcdias127@gmail.com>
2015-06-09 12:18:27 +0100

    Default reward value when reward selected

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,6 +1,7 @@
 class PledgesController < ApplicationController
   	before_action :authenticate_user!
   	before_action :set_project
+  	before_action :set_reward
 
   	def index
   		@pledges = @project.pledges
@@ -21,5 +22,9 @@ class PledgesController < ApplicationController
   	
   	def set_project
 		@project = Project.find(params[:project_id])
+	end
+
+	def set_reward		
+	    @reward = @project.rewards.find_by_id(params[:reward_id])
 	end
 end
